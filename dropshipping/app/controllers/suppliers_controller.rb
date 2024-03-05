18c00481467d9d07dedf767d979ed3e857c5d604
leuhsouza <leuhsouza@gmail.com>
2024-03-05 09:14:41 -0300

    feat: add suppliers controller

--- dropshipping/app/controllers/suppliers_controller.rb
+++ dropshipping/app/controllers/suppliers_controller.rb
@@ -16,6 +16,35 @@ class SuppliersController < ApplicationController
   end
 
   def create
+    @supplier = Supplier.new(supplier_params)
 
+    if @supplier.save
+      redirect_to @supplier, notice: 'Supplier was successfully created.'
+    else
+      render :new
+    end
   end
+
+  def update
+    if @supplier.update(supplier_params)
+      redirect_to @supplier, notice: 'Supplier was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @supplier.destroy
+    redirect_to suppliers_url, notice: 'Supplier was successfuly destroyed.'
+  end
+
+  private
+    def set_supplier
+      @supplier = Supplier.find(params[:id])
+    end
+
+  def supplier_params
+    params.require(:supplier). permit(:name)
+  end
+
 end
