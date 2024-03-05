216e21cb347c75948d0747cc9c6b63be1ddf8d55
leuhsouza <leuhsouza@gmail.com>
2024-03-05 08:45:42 -0300

    wip:create controllers

---
+++ dropshipping/app/controllers/suppliers_controller.rb
@@ -0,0 +1,21 @@
+class SuppliersController < ApplicationController
+  before_action :set_supplier, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @suppliers = Supplier.all
+  end
+
+  def show
+  end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def edit
+  end
+
+  def create
+
+  end
+end
